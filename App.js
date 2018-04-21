aa0b957147d303875c13d9a85bb529e7c437444a
RTG™ <igadget28@gmail.com>
2018-04-21 11:23:53 +0800

    Renamed to Instagram-Feed

--- App.js
+++ App.js
@@ -1,63 +1,157 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  FlatList,
+} from 'react-native'
+import { Constants, AppLoading } from 'expo'
 
-export default class App extends React.Component {
+export default class App extends Component {
+  // This is the access_token for our demo instagram account
+  // To generate your own, each user needs to authorize your app to
+  // access their account.
+  access_token = '1755587115.b154a7e.3f9de4a1c3be4d4790a170171633075e'
+  // access_token2 = '638509013.cbba754.fd61c28f8fd44c3787620c4f35376325'
+  user_id = '1755587115'
+
   state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <RootNavigation />
-        </View>
-      );
-    }
+    loaded: false,
+    data: null,
+    comments: [],
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+  componentDidMount () {
+    this.fetchFeed()
+  }
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+  async fetchFeed () {
+    let response = await fetch(
+      'https://api.instagram.com/v1/users/' +
+      this.user_id +
+      '/media/recent/?access_token=' +
+      this.access_token
+    )
+    let posts = await response.json()
+    let comments = await this.makeCommentsList(posts.data)
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    this.setState({
+        data: posts.data,
+        comments: comments,
+        loaded: true
+    })
+  }
+
+  createPost(postInfo, index) {
+    let imageUri = postInfo.images.standard_resolution.url
+    let username = postInfo.user.username
+    let numLikes = postInfo.likes.count
+
+    return (
+      <View>
+        <Image style={styles.image} source={{ uri: imageUri }} />
+        <View style={styles.info}>
+          <Text style={styles.infoText}>{username}</Text>
+          <Text style={styles.infoText}>
+            {numLikes + (numLikes !== 1 ? ' likes' : ' like')}
+          </Text>
+        </View>
+        <View>
+          {this.state.comments[index]}
+        </View>
+      </View>
+    )
+  }
+
+  async makeCommentsList(posts) {
+    let postsArray = posts.map(async (post) => {
+      let postId = post.id
+
+      if (post.comments.count === 0) {
+        return (
+          <View style={styles.comment} key={postId}>
+            <Text>No Comments!</Text>
+          </View>
+        )
+      } else {
+        let response = await fetch(
+          'https://api.instagram.com/v1/media/' +
+            postId +
+            '/comments?access_token=' +
+            this.access_token
+        )
+        let comments = await response.json()
+        let commentsArray = comments.data
+        // console.log(commentsArray)
+        let commentsList = commentsArray.map(commentInfo => {
+          return (
+            <View style={styles.comment} key={commentInfo.id}>
+              <Text style={styles.commentText}>{commentInfo.from.username}</Text>
+              <Text>{commentInfo.text}</Text>
+            </View>
+          )
+        })
+        return commentsList
+      }
+    })
+
+    postsArray = await Promise.all(postsArray)
+    return postsArray
+  }
+
+  render () {
+    if (!this.state.loaded) {
+      return (
+        <AppLoading />
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item, index }) => this.createPost(item, index)}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
-});
+  image: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width,
+  },
+  info: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderBottomWidth: 1,
+    borderColor: '#d8d8d8',
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  comment: {
+    flexDirection: 'row',
+    padding: 10,
+    paddingLeft: 15,
+    borderBottomWidth: 1,
+    borderColor: '#d8d8d8',
+  },
+  commentText: {
+    paddingRight: 15,
+    fontWeight: 'bold',
+  },
+})
